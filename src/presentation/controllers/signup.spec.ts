6bebdeff03b42394aa18cca9e3e4d92358688134
João Henrique Inacio <jriquee@gmail.com>
2022-02-09 11:31:54 -0300

    fix: fix imports

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,8 +1,8 @@
 import { SignUpController } from './signup'
 import { MissingParamError, InvalidParamError, ServerError } from '../errors'
 import { EmailValidator } from '../protocols'
-import { AccountModel } from '../domain/models/account'
-import { AddAccount, AddAccountModel } from '../domain/models/add-account'
+import { AccountModel } from '../../domain/models/account'
+import { AddAccount, AddAccountModel } from '../../domain/usecases/add-account'
 
 const makeEmailValidator = (): EmailValidator => {
   class EmailValidatorStub implements EmailValidator {
